f58799be323f386700f0672681d4c6fde953359f
2021036835 <baeyoujung1@hanyang.ac.kr>
2022-05-24 23:34:44 +0900

    Quizz1

---
+++ hw/Quizz.java
@@ -0,0 +1,60 @@
+package hw;
+
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+public class Quizz {
+   public static void main(String args[]) {
+      while(true) {
+         String path = System.getProperty("user.dir");
+         System.out.print("Command: ");
+         Scanner scannerObject = new Scanner(System.in);
+         String command = scannerObject.next();
+         
+         if (command.equals("pwd")) {
+            System.out.println(path);
+         }
+         else if (command.equals("list")) {
+            String[] str =  new File(path).list();
+            for(String lis : str) {  
+                String file2 = path + "\\" + lis;
+                File file = new File(file2);          
+                String info = ""; 
+                String folder = file.isDirectory() ? "d" : "-";
+                String execute = file.canExecute() ? "x" : "-";
+                String read = file.canRead() ? "r" : "-";
+                String write = file.canWrite() ? "w" : "-";
+                    
+                long size = folder.equals("d") ? 512 : file.length();            
+                info = folder + execute + read + write;       
+                System.out.print(info);
+                System.out.printf("%12d", size);
+                System.out.print("        ");
+                System.out.print(lis);
+                System.out.println();
+            }
+         }
+         else {
+            try {
+               File file = new File(command);
+               FileReader file3 = new FileReader(file);
+               int count = 0;
+               while((count = file3.read()) != -1) {
+                  System.out.print((char)count);
+               }
+               System.out.println();
+               file3.close();
+            }
+            catch(FileNotFoundException e) {
+               System.out.println("No such a file: " + command);
+            }
+            catch(IOException e) {
+               e.getStackTrace();
+            }
+         }
+      }
+   }
+}
